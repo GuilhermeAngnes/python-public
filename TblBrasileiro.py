9949586d10995b369370f40e34ab4403dd7e2b09
GuilhermeAngnes <guilhermeangnes13@gmail.com>
2022-10-05 23:30:35 -0300

    Update TblBrasileiro.py

--- TblBrasileiro.py
+++ TblBrasileiro.py
@@ -1,19 +1,24 @@
 from requests_html import HTMLSession
+import pandas as pd
+import requests
+
 
 session = HTMLSession()
 
 requisition = session.get(
     "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/"
-    )
+)
 
 nome_times = requisition.html.find(".table__team")
 
 pontos = requisition.html.find(".table__stats")
 
-#print(nome_times[1].text)
+titulo_time = requisition.html.find(".table__team")
+titulo_pontos = requisition.html.find(".table__stats")
 
-#print(pontos[9].text)
+print(titulo_time[0].text + " " + titulo_pontos[0].text)
 
 for item in range(20):
-    print(nome_times[item + 1].text + " " + pontos[item + 9].text)
-
+    dados = (nome_times[item + 1].text + " " + pontos[item + 9].text)
+    print(dados)
+    # dados.to_excel('dados.xls')
